a4c2b48bcaa6deae98a76567650a3f1c1eb595ff
Chandler Abril <abrilchandler1@gmail.com>
2023-08-08 12:14:58 -0700

    Starting to understand some aspects

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+let currentUser = 'Grace Hopper';
+currentUser = 'GRACE HOPPER';
+
+const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser + "!";
+
+const shortGreeting = "Welcome, " + currentUser[0] + "!";
